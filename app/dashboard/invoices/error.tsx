efb93f9a630d010378759bd1c090cb93c19198b1
andreluizpo <andreluizoliveira163@hotmail.com>
2026-03-13 15:52:53 -0400

    Aprendendo sobre o assunto Tratamento de Erros

---
+++ app/dashboard/invoices/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    // Opcionalmente, registre o erro em um serviço de relatório de erros.
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center">Algo deu errado!</h2>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Tentativa de recuperação: renderizar novamente a rota de faturas
+          () => reset()
+        }
+      >
+        Tente novamente
+      </button>
+    </main>
+  );
+}
--- app/lib/action.ts
+++ app/lib/action.ts
@@ -27,9 +27,17 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0]; // Formata a data em "YYYY-MM-DD"
 
-  await sql`INSERT INTO invoices (customer_id, amount, status, date)
+  // Tenta fazer a inserir os dados de uma nova fatura no BD. Caso contrario, retorna uma mensagem de erro.
+  try {
+    await sql`INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Erro no banco de dados: Falha ao criar a fatura.",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -44,17 +52,26 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    console.error(error);
+    return {
+      message: "Erro no banco de dados: Falha ao atualizar a fatura.",
+    };
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error("Erro ao deletar fatura");
+
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
